9194666ef70ebc90d6749133a63bcde85d099530
Meji Amin <mokhmadmishiev@gmail.com>
2025-03-05 19:37:35 +0300

    first commit

---
+++ js/data.js
@@ -0,0 +1,16 @@
+
+// data.js
+
+const cardData = {
+    name: 'Катер Meridian 401',
+    description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
+    img: 'img/photo-card-1.jpg',
+    price: 14000,
+    features: [
+        '2 каюты',
+        '4 спальных места',
+        'джакузи',
+    ]
+};
+
+window.cardData = cardData;
