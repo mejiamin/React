7149c6dc62a92822f29ce090ffacea4ab478c57a
Meji Amin <mokhmadmishiev@gmail.com>
2025-03-05 20:48:55 +0300

    1.10. parameters and JSX - 10 from 15

--- js/data.js
+++ js/data.js
@@ -1,16 +1,52 @@
+const cards = [
+    {
+        id: '1',
+        name: 'Катер Meridian 401',
+        description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
+        img: 'img/photo-card-1.jpg',
+        price: 14000,
+        features: [
+            '2 каюты',
+            '4 спальных места',
+            'аудиосистема, кондиционер',
+        ],
+    },
+    {
+        id: '2',
+        name: 'Катер 60 Manhattan',
+        description: 'Двухпалубное судно VIP уровня длиной 20 метров',
+        img: 'img/photo-card-2.jpg',
+        price: 25000,
+        features: [
+            '6 кают',
+            'оборудованная кухня',
+            'джакузи',
+        ],
+    },
+    {
+        id: '3',
+        name: 'Катер Starfisher 34',
+        description: 'Моторная яхта длиной 11 метров и вместимостью до 8 человек',
+        img: 'img/photo-card-3.jpg',
+        price: 18000,
+        features: [
+            '4 каюты',
+            '6 спальных мест',
+            'аудиосистема, кондиционер',
+        ],
+    },
+    {
+        id: '4',
+        name: 'Катер Atlantic 3000',
+        description: 'Катер Atlantic длиной 14 метров и вместимостью до 6 человек',
+        img: 'img/photo-card-4.jpg',
+        price: 15000,
+        features: [
+            '2 каюты',
+            '4 спальных места',
+            'оборудованная кухня',
+        ],
+    },
+];
 
-// data.js
-
-const cardData = {
-    name: 'Катер Meridian 401',
-    description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
-    img: 'img/photo-card-1.jpg',
-    price: 14000,
-    features: [
-        '2 каюты',
-        '4 спальных места',
-        'джакузи',
-    ]
-};
-
-window.cardData = cardData;
+window.cards = cards;
