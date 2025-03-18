9cb1900a5b8a4507710b61e00b2e2592d934c149
Meji Amin <mokhmadmishiev@gmail.com>
2025-03-18 12:50:14 +0300

    Learned 1.5/1.11 from 6.4

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 // script.js
 
+
+
+// const person = {name: "Jon", age: 30, gender: "male"};
+
+// function printPerson({name, age, gender}) {
+//     console.log(name + ' ' + age + ' ' + gender);
+// }
+
+// printPerson(person);
+
+
+
+// const colors = ["red", "green", "blue"];
+
+// function printColors([first, second, third]) {
+//     console.log(second, first, third);
+// }
+
+// printColors(colors);
+
+
+
+function printName({name = "Jon", age = 27}) {
+    console.log(name + ' ' + age);
+}
+
+printName({age: 30});
+printName({name: "Mike", age: 35});
