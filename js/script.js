d9ef5a2dbe646f8f0e1c8591085bc735a2657c86
Meji Amin <mokhmadmishiev@gmail.com>
2025-03-20 13:16:11 +0300

    A little 1.5/1.11 from 6.4

--- js/script.js
+++ js/script.js
@@ -2,29 +2,102 @@
 
 
 
-// const person = {name: "Jon", age: 30, gender: "male"};
 
-// function printPerson({name, age, gender}) {
-//     console.log(name + ' ' + age + ' ' + gender);
+// var handleToggle = document.querySelector('.open');
+
+// const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(container);
+
+// function MyComponent(props) {
+//     const { onToggle, isOpen } = props;
+//     return (
+//         <div className={isOpen ? 'open' : ''} onClick={onToggle}>Component</div>
+//     )
 // }
 
-// printPerson(person);
+// root.render(<MyComponent onClick={handleToggle} isOpen={true} />)
 
 
 
-// const colors = ["red", "green", "blue"];
 
-// function printColors([first, second, third]) {
-//     console.log(second, first, third);
+// function UserCard(props) {
+//     return (
+//         <div>
+//             <h2>{props.name}</h2>
+//             <p>{props.email}</p>
+//             <p>{props.phone}</p>
+//         </div>
+//     )
 // }
 
-// printColors(colors);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<UserCard name={'Amin'} email={'amin@mail.com'} phone={'123-456-789'} />)
 
 
 
-function printName({name = "Jon", age = 27}) {
-    console.log(name + ' ' + age);
+
+// function UserCard({name, email, phone}) {
+//     return (
+//         <div>
+//             <h2>{name}</h2>
+//             <p>{email}</p>
+//             <p>{phone}</p>
+//         </div>
+//     )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<UserCard name={'Amin'} email={'amin@mail.com'} phone={'123-456-789'} />)
+
+
+
+
+// function Example() {
+//     const [count, setCount] = React.useState(0);
+
+//     return (
+//         <div>
+//             <p>Вы нажали {count} раз</p>
+//             <button onClick={() => setCount(count + 1)}>
+//                 Нажми
+//             </button>
+//         </div>
+//     )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Example />)
+
+
+
+
+// function Example() {
+//     const [count, setCount] = React.useState(0);
+//     const [name, setName] = React.useState("Jon");
+
+//     return (
+//         <div>
+//             <p>Вы нажали {count} раз</p>
+//             <p>Вас зовут {name}</p>
+//             <button onClick={() => setCount(count + 1)}>Нажми</button>
+//             <button onClick={() => setName("Jane")}>Изменить имя</button>
+//         </div>
+//     )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Example />);
+
+
+
+
+const Button = (props) => {
+    return <button {...props}>Нажми на меня!</button>
 }
 
-printName({age: 30});
-printName({name: "Mike", age: 35});
+const App = () => {
+    return <Button className="btn btn-pripary" onClick={() => console.log("Нажали!")} />
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(App());
