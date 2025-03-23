ef379f325606e4ba10dbacb6ae4f75864dda7e43
Meji Amin <mokhmadmishiev@gmail.com>
2025-03-23 14:51:47 +0300

    Выучил React. Основы синтаксиса в 1.8/1.11 of 6.4

--- js/script.js
+++ js/script.js
@@ -1,23 +1,49 @@
 // script.js
 
-// const containerElement = document.getElementById('root');
 
-// function Message({from}) {
-//     return <p>Вам письмо от <b>{from}</b></p>
-// };
 
-// function App({children}) {
+
+// const container = document.getElementById('root');
+
+// function MyComponent({ title, className, expression, children }) {
 //     return (
-//         <section>
-//             <Message from="Keks" />
-//             {children}
-//         </section>
+//         <div className={className}>
+//             <h1 className="title">{title}</h1>
+//             <p className="description">Мой первый компонент</p>
+//             <span className="expression">{children} = {expression}</span>
+//             <br/>
+//             <img src="" alt="Мне нужен url" />
+//         </div>
 //     );
-// };
+// }
 
-// const root = ReactDOM.createRoot(containerElement);
+// const root = ReactDOM.createRoot(container);
 // root.render(
-//     <App>
-//         <p>Это дочерний компонент</p>
-//     </App>
+//     <MyComponent
+//         title="Hello React!"
+//         className="container"
+//         expression={2 + 2}
+//     >
+//         <span>2 + 2</span>
+//     </MyComponent>
 // );
+
+
+
+
+
+const container = document.getElementById('root');
+
+function MyComponent({ title, className, expression, children }) {
+    return (
+        React.createElement('div', { className: 'container' }, [
+            React.createElement('h1', { className: 'title' }, title),
+            React.createElement('img', { src: '', alt: 'Мне нужен url' })
+        ])
+    );
+}
+
+const root = ReactDOM.createRoot(container);
+root.render(
+    React.createElement(MyComponent, {title: 'Приложение на React без JSX'})
+);
