230fbd7f0acbde3b51b768ec2b8fe21f07a9e7db
Meji Amin <mokhmadmishiev@gmail.com>
2025-03-24 15:16:30 +0300

    Выучил Различию JSX и HTML  в 1.9/1.11 of 6.4

--- js/script.js
+++ js/script.js
@@ -1,49 +1,17 @@
 // script.js
 
-
-
-
-// const container = document.getElementById('root');
-
-// function MyComponent({ title, className, expression, children }) {
-//     return (
-//         <div className={className}>
-//             <h1 className="title">{title}</h1>
-//             <p className="description">Мой первый компонент</p>
-//             <span className="expression">{children} = {expression}</span>
-//             <br/>
-//             <img src="" alt="Мне нужен url" />
-//         </div>
-//     );
-// }
-
-// const root = ReactDOM.createRoot(container);
-// root.render(
-//     <MyComponent
-//         title="Hello React!"
-//         className="container"
-//         expression={2 + 2}
-//     >
-//         <span>2 + 2</span>
-//     </MyComponent>
-// );
-
-
-
-
-
 const container = document.getElementById('root');
 
-function MyComponent({ title, className, expression, children }) {
+function App() {
     return (
-        React.createElement('div', { className: 'container' }, [
-            React.createElement('h1', { className: 'title' }, title),
-            React.createElement('img', { src: '', alt: 'Мне нужен url' })
-        ])
+        <React.Fragment>
+            <h1>Hello</h1>
+            <form>
+                <input type="text" required />
+            </form>
+        </React.Fragment>
     );
 }
 
 const root = ReactDOM.createRoot(container);
-root.render(
-    React.createElement(MyComponent, {title: 'Приложение на React без JSX'})
-);
+root.render(<App />);
