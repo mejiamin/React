3d84ac9ccc96c28a262aed4554a8c06e91cb61c4
Meji Amin <mokhmadmishiev@gmail.com>
2025-03-27 14:51:04 +0300

    Выучил Параметры и JSX в 1.10/1.12 of 6.4

--- js/data.js
+++ js/data.js
@@ -1,83 +1,31 @@
 // data.js
 
-const words = [
+const isTopSellers = ['1', '2'];
+const cards = [
     {
         id: '1',
-        token: 'house',
-        word: 'Сasa',
-        type: 'word'
+        name: 'Катер Meridian 401',
+        description: 'Яхта 401 длиной 12.5 метров и шириной 4.2 метра',
+        img: 'img/photo-card-1.jpg',
+        price: 14000,
+        features: [
+            '2 каюты',
+            '4 спальных места',
+            'аудиосистема, кондиционер'
+        ]
     },
     {
         id: '2',
-        token: 'house',
-        url: 'img/photo-card-4.jpg',
-        type: 'image'
-    },
-    {
-        id: '3',
-        token: 'dog',
-        word: 'Сane',
-        type: 'word'
-    },
-    {
-        id: '4',
-        token: 'dog',
-        url: 'img/photo-card-6.jpg',
-        type: 'image'
-    },
-    {
-        id: '5',
-        token: 'teapot',
-        word: 'Teiera',
-        type: 'word'
-    },
-    {
-        id: '6',
-        token: 'teapot',
-        url: 'img/photo-card-5.jpg',
-        type: 'image'
-    },
-    {
-        id: '7',
-        token: 'bed',
-        word: 'Letto',
-        type: 'word'
-    },
-    {
-        id: '8',
-        token: 'bed',
-        url: 'img/photo-card-2.jpg',
-        type: 'image'
-    },
-    {
-        id: '9',
-        token: 'headphone',
-        word: 'Cuffia',
-        type: 'word'
-    },
-    {
-        id: '10',
-        token: 'headphone',
-        url: 'img/photo-card-1.jpg',
-        type: 'image'
-    },
-    {
-        id: '11',
-        token: 'laptop',
-        word: 'Portatile',
-        type: 'word'
-    },
-    {
-        id: '12',
-        token: 'laptop',
-        url: 'img/photo-card-3.jpg',
-        type: 'image'
+        name: 'Катер 60 Manhattan',
+        description: 'Двухпалубное судно VIP уровня длиной 20 метров',
+        img: 'img/photo-card-2.jpg',
+        price: 25000,
+        features: [
+            '6 кают',
+            'оборудованная кухня',
+            'джакузи'
+        ]
     }
 ];
 
-const selectedItems = ['3', '4'];
-const finishedItems = ['11', '12'];
-
-window.words = words;
-window.selectedItems = selectedItems;
-window.finishedItems = finishedItems;
+window.cards = cards;
